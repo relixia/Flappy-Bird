5f8279b086b28d402e04f8fcf3406b73fb15d8ad
Buğra Çayır <77904399+relixia@users.noreply.github.com>
2022-06-13 23:46:50 +0300

    Add files via upload

---
+++ Assets/Scripts/Birdy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdy : MonoBehaviour
+{
+    public float velocity = 1f;
+    public Rigidbody2D rb2D;
+
+    public bool isDead;
+
+    public GameManager managerGame;
+
+    public GameObject DeathScreen;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tıklamayı al
+        if (Input.GetMouseButtonDown(0))
+        {
+            //havada kuşu sıçrat
+            rb2D.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "ScoreArea")
+        {
+            managerGame.UpdateScore();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "DeathArea")
+        {
+            isDead = true;
+            Time.timeScale = 0;
+
+            DeathScreen.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
